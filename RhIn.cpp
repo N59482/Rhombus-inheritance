ba144b852ff6ca939c5858bff7f5bfb21ebe6466
N59482 <iduginmail@ya.ru>
2021-12-17 17:43:52 +0300

    Программа для демонстрации ромбовидного наслодования

--- RhIn.cpp
+++ RhIn.cpp
@@ -1,190 +1,154 @@
 #include <iostream>
-#include <cstdlib>
+#include <ctime>
 #include <string>
 
 using namespace std;
 
-
+/*
+* Родительский класс Human
+* содержит информацию о имени, цвете глаз, росте и весе.
+*/
 class Human
-{
-	
-	public:
-   Human()
-   {
-	   cout<<"Конструктор класса human"<<endl;
-   };
+	{
+		public:
+		Human()
+		   {
+			   cout<<"Запущен конструктор класса human"<<endl;
+		   };
+		void Show()
+			{
+			cout<<"Имя\t"<<this->name<<"\nГендер\t"<<this->gender<<"\nЦв глаз\t"<<this->eyecolor<<"\nРост\t"<<this->height<<"\nВес\t"<<this->weight<<endl<<endl;
+			};
 		
-	void Show()
-	{
-	cout<<"Имя\t"<<this->name<<"\nГендер\t"<<this->gender<<"\nЦв глаз\t"<<this->eyecolor<<"\nРост\t"<<this->rost<<"\nВес\t"<<this->wes<<endl<<endl;
-	};
-	
-	string Getcolor()
-	{
-		return this->eyecolor;
-	};
+		string Getcolor()
+			{
+				return this->eyecolor;
+			};
+		int Getheight()
+			{
+				return this->height;
+			};
+		int Getweight()
+			{
+				return this->weight;
+			};
 		
-	string Getgender()
-	{
-		return this->gender;
+		protected:
+		string gender;
+		string name;
+		string eyecolor;
+		int height;
+		int weight;
 	};
-	
-	int Getrost()
-	{
-		return this->rost;
-	};
-	
-	int Getwes()
-	{
-		return this-wes;
-	};
-	
-	protected:
-	string gender;
-	string name;
-	string eyecolor;
-	int rost;
-	int wes;
-		
-};
-
-
+/*
+* Дочерние классы Man и Woman содержат инфрмацию о гендере.
+* Имею комплекс делигирующих конструкторов для заполнения полей класса Human полностью или частично.
+*/
 class Woman : public virtual Human
-{
-	public:
-	
-	
-	Woman()
 	{
-	    this->name = "нет данных";
-		this->eyecolor = "нет данных";
-		this->rost = 0;
-		this->wes = 0;
-		this->gender="Девушка";
-	}
-	Woman (string name):Woman()
-	{
-		this->name = name;
-	};
+		public:
+		Woman()
+			{
+			    this->name = "нет данных";
+				this->eyecolor = "нет данных";
+				this->height = 0;
+				this->weight = 0;
+				this->gender = "Девушка";
+			}
+		Woman (string name):Woman()
+			{
+				this->name = name;
+			};
 		Woman (string name, string eyecolor ):Woman(name)
-	{
-		this->eyecolor = eyecolor;
+			{
+				this->eyecolor = eyecolor;
+			};
+		Woman (string name, string eyecolor, int height):Woman(name, eyecolor)
+			{
+				this->height = height ;
+			};
+		Woman (string name, string eyecolor , int height , int weight): Woman (name, eyecolor , height )
+			{
+				this->weight=weight;
+			};
+		string Getgender()
+			{
+				return this->gender;
+			};
 	};
-		Woman (string name, string eyecolor, int rost):Woman(name, eyecolor)
-	{
-		this->rost = rost ;
-	};
-	Woman (string name, string eyecolor , int rost , int wes): Woman (name, eyecolor , rost )
-	{
-		this->wes=wes;
-	};
-	
-	void Love(Human &other)
-	{
-		cout<<"Я влюбилась в его  "<<other.Getcolor()<<" глаза!"<<endl;
-	};
-
-	private:
-	protected:
-};
-
 
 class Man:  public virtual Human
-{
-	public:
+	{
+		public:
 		Man()
-	{
-	    this->name = "нет данных";
-		this->eyecolor = "нет данных ";
-		this->rost = 0;
-		this->wes = 0;
-		this->gender="Парень";
-	}
-	Man (string name):Man()
-	{
-		this->name = name;
-	};
+			{
+			    this->name = "нет данных";
+				this->eyecolor = "нет данных ";
+				this->height = 0;
+				this->weight = 0;
+				this->gender = "Парень";
+			}
+		Man (string name):Man()
+			{
+				this->name = name;
+			};
 		Man (string name, string eyecolor ):Man(name)
-	{
-		this->eyecolor = eyecolor;
+			{
+				this->eyecolor = eyecolor;
+			};
+		Man (string name, string eyecolor, int height):Man(name,  eyecolor)
+			{
+				this->height = height ;
+			};
+		Man (string name,string eyecolor , int height , int weight): Man (name,  eyecolor , height )
+			{
+				this->weight=weight;
+			};
+		string Getgender()
+			{
+				return this->gender;
+			};
 	};
-		Man (string name, string eyecolor, int rost):Man(name,  eyecolor)
-	{
-		this->rost = rost ;
-	};
-	Man (string name,string eyecolor , int rost , int wes): Man (name,  eyecolor , rost )
-	{
-		this->wes=wes;
-	};
-	
-	void Love(Human &other)
-	{
-		cout<<"Я влюбился в её "<<other.Getcolor()<<" глаза!"<<endl;
-	};
-	
-	private:
-	protected:
-};
 
 
-
+/*
+* Класс Child получен путём ромбовидного наследования от классов Woman и Man
+* Класс имеет доступ к функциям класса Human и может оперировать информацией хранящейся в классах Woman и Man
+*/
 class Child: public Woman, public Man
-{
-	public:
-	Child(string sname, Woman &Mom, Man &Dad)
 	{
-	this->name=sname;
-	
-	if (Randfoo())
-	{
-		 this->gender=Dad.Getgender();
-	} else this->gender=Mom.Getgender();
-
-	if (Randfoo())
-	{
-		 this->eyecolor=Dad.Getcolor();
-	} else this->eyecolor=Mom.Getcolor();
-	
-	if (Randfoo())
-	{
-		 this->wes=Dad.Getwes();
-	} else this->wes=Mom.Getwes();
-    
-	
-	if (Randfoo())
-	{
-		 this->rost=Dad.Getrost();
-	} else this->rost=Mom.Getrost();
-	
-	
-	
+		public:
+		Child(string sname, Woman &Mom, Man &Dad)
+			{
+				this->name = sname;
+				if (Randfoo()) this->gender=Dad.Getgender();
+					else this->gender=Mom.Getgender();
+				if (Randfoo()) this->eyecolor=Dad.Getcolor();
+					else this->eyecolor=Mom.Getcolor();
+				if (Randfoo()) this->weight=Dad.Getweight();
+					else this->weight=Mom.Getweight();
+				if (Randfoo()) this->height=Dad.Getheight();
+					else this->height=Mom.Getheight();
+			};
+		bool Randfoo()
+			{
+				return rand() % 2;
+			};
 	};
-	
-	bool Randfoo()
-	{
-		bool res;
-		if ((rand()/2)==0) res=0;
-		else res=1;
-		
-		return res;
-	};
-	
-	private:
-	protected:
-};
 
 int main()
-{
-    Woman Nika("Ника", "почти_зеленые", 161, 80);
-	Nika.Show();
-	
-	Man Nikita("Никита","синие", 180  ,80 );
-	Nikita.Show();
-	
-	Child Kira("Саша", Nika , Nikita);
-	Kira.Show();
-     return 0;
-}
+	{
+		srand(time(0));
+	    Woman Maria("Мария", "почти_зеленые", 161, 55);
+		Maria.Show();
+		
+		Man Stepan("Степан","синие", 180  ,80 );
+		Stepan.Show();
+		
+		Child Kira("Саша", Maria, Stepan);
+		Kira.Show();
+	     return 0;
+	}
 
 
 
